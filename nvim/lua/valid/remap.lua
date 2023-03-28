973877852129592ed40450e65585f06d8622dd58
Colby <colby.leiske@gmail.com>
2023-03-27 22:41:40 -0700

    fix telescope

--- nvim/lua/valid/remap.lua
+++ nvim/lua/valid/remap.lua
@@ -1,3 +1,5 @@
+local wk = require("which-key")
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -41,9 +43,19 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+-- vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+wk.register({
+  ["<leader>"] = {
+    s = {
+      name = "search",
+      f = {require('telescope.builtin').find_files, '[S]earch [F]iles'},
+      -- r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+      -- n = { "<cmd>enew<cr>", "New File" },
+    },
+  },
+})
