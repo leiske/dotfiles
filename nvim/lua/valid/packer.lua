d88623ee9d1a812ef22a912f141351d1f5592715
Colby <colby.leiske@gmail.com>
2023-03-30 22:02:35 -0700

    sloppily try to switch to lazy

--- nvim/lua/valid/packer.lua
+++ nvim/lua/valid/packer.lua
@@ -1,74 +1,125 @@
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
---  use({ 
---	  'rose-pine/neovim',
---	  as = 'rose-pine',
---	  config = function()
---		  vim.cmd('colorscheme rose-pine')
---	  end
---  })
-
-  use({ 
-	  'sainnhe/everforest',
-	  as = 'everforest',
-	  config = function()
-		  vim.cmd('colorscheme everforest')
-	  end
+-- Install lazy.nvim package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
+end
+vim.opt.rtp:prepend(lazypath)
 
-	use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
+return require('lazy').setup({
+  'wbthomason/packer.nvim',
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  'nvim-treesitter/playground',
+  'ThePrimeagen/harpoon',
+  'ThePrimeagen/vim-be-good',
+  'mbbill/undotree',
+  'tpope/vim-fugitive',
+  'tpope/vim-surround',
+  'tpope/vim-commentary',
+  'tpope/vim-sleuth',
+  'echasnovski/mini.pairs',
+  'folke/which-key.nvim', 
+  'j-hui/fidget.nvim',
 
-	use 'nvim-treesitter/nvim-treesitter-textobjects'
-	use 'nvim-treesitter/playground'
-	use 'ThePrimeagen/harpoon'
-	use 'ThePrimeagen/vim-be-good'
-	use 'mbbill/undotree'
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-surround'
-	use 'tpope/vim-commentary'
-	use 'echasnovski/mini.pairs'
-	use 'tpope/vim-sleuth'
-	
-	-- Pending keybinds
-	use 'folke/which-key.nvim'
-	
-	-- status updates for lsp
-	use 'j-hui/fidget.nvim'
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+      'williamboman/mason.nvim',
+      build = function()
+	pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	-- use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+      'williamboman/mason.nvim',
+      build = function()
+	pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-	use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-	    -- LSP Support
-	    {'neovim/nvim-lspconfig'},             -- Required
-	    {'williamboman/mason.nvim'},           -- Optional
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'nvim-treesitter/nvim-treesitter', 
+    build = function()
+      pcall(vim.cmd, 'TSUpdate')
+    end,
+  }
+}
+})
+-- use {
+  --   'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  --   -- or                            , branch = '0.1.x',
+  --   requires = { {'nvim-lua/plenary.nvim'} }
+  -- }
 
-	    -- Autocompletion
-	    {'hrsh7th/nvim-cmp'},         -- Required
-	    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-	    {'hrsh7th/cmp-buffer'},       -- Optional
-	    {'hrsh7th/cmp-path'},         -- Optional
-	    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-	    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+  --  use({ 
+    --	  'rose-pine/neovim',
+    --	  as = 'rose-pine',
+    --	  config = function()
+      --		  vim.cmd('colorscheme rose-pine')
+      --	  end
+      --  })
 
-	    -- Snippets
-	    {'L3MON4D3/LuaSnip'},             -- Required
-	    {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-	}
-end)
+      -- use({ 
+	--   'sainnhe/everforest',
+	--   as = 'everforest',
+	--   config = function()
+	  -- vim.cmd('colorscheme everforest')
+	  --   end
+	  -- })
+
+	  -- use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
+
+
+	  -- -- use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+	  -- use {
+	    --   'VonHeikemen/lsp-zero.nvim',
+	    --   branch = 'v1.x',
+	    --   requires = {
+	      -- -- LSP Support
+	      -- {'neovim/nvim-lspconfig'},             -- Required
+	      -- {'williamboman/mason.nvim'},           -- Optional
+	      -- {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	      -- -- Autocompletion
+	      -- {'hrsh7th/nvim-cmp'},         -- Required
+	      -- {'hrsh7th/cmp-nvim-lsp'},     -- Required
+	      -- {'hrsh7th/cmp-buffer'},       -- Optional
+	      -- {'hrsh7th/cmp-path'},         -- Optional
+	      -- {'saadparwaiz1/cmp_luasnip'}, -- Optional
+	      -- {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+	      -- -- Snippets
+	      -- {'L3MON4D3/LuaSnip'},             -- Required
+	      -- {'rafamadriz/friendly-snippets'}, -- Optional
+	      --   }
+	      -- }
+	      -- end)
