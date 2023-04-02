81451ad8be79d7c18f256c85892578b50c9dd8dc
Colby <colby.leiske@gmail.com>
2023-04-01 21:00:38 -0700

    try8ing to get rainbows wokring, doesnt

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,4 +1,5 @@
-require('nvim-treesitter.configs').setup {
+  print('foo')
+require('nvim-treesitter.configs').setup ({
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "help", "query" },
 
@@ -28,7 +29,7 @@ require('nvim-treesitter.configs').setup {
     -- Which query to use for finding delimiters
     query = 'rainbow-parens',
     -- Highlight the entire buffer all at once
-    strategy = require('ts-rainbow').strategy.global,
+    -- strategy = require('ts-rainbow').strategy.global,
     hlgroups = {
       'TSRainbowYellow',
       'TSRainbowBlue',
@@ -39,57 +40,57 @@ require('nvim-treesitter.configs').setup {
     },
 
   },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
-      scope_incremental = '<c-s>',
-      node_decremental = '<M-space>',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>a'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
-      },
-    },
-  },
-}
+  -- incremental_selection = {
+  --   enable = true,
+  --   keymaps = {
+  --     init_selection = '<c-space>',
+  --     node_incremental = '<c-space>',
+  --     scope_incremental = '<c-s>',
+  --     node_decremental = '<M-space>',
+  --   },
+  -- },
+  -- textobjects = {
+  --   select = {
+  --     enable = true,
+  --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+  --     keymaps = {
+  --       -- You can use the capture groups defined in textobjects.scm
+  --       ['aa'] = '@parameter.outer',
+  --       ['ia'] = '@parameter.inner',
+  --       ['af'] = '@function.outer',
+  --       ['if'] = '@function.inner',
+  --       ['ac'] = '@class.outer',
+  --       ['ic'] = '@class.inner',
+  --     },
+  --   },
+  --   move = {
+  --     enable = true,
+  --     set_jumps = true, -- whether to set jumps in the jumplist
+  --     goto_next_start = {
+  --       [']m'] = '@function.outer',
+  --       [']]'] = '@class.outer',
+  --     },
+  --     goto_next_end = {
+  --       [']M'] = '@function.outer',
+  --       [']['] = '@class.outer',
+  --     },
+  --     goto_previous_start = {
+  --       ['[m'] = '@function.outer',
+  --       ['[['] = '@class.outer',
+  --     },
+  --     goto_previous_end = {
+  --       ['[M'] = '@function.outer',
+  --       ['[]'] = '@class.outer',
+  --     },
+  --   },
+  --   swap = {
+  --     enable = true,
+  --     swap_next = {
+  --       ['<leader>a'] = '@parameter.inner',
+  --     },
+  --     swap_previous = {
+  --       ['<leader>A'] = '@parameter.inner',
+  --     },
+  --   },
+  -- },
+});
--- nvim/lua/valid/packer.lua
+++ nvim/lua/valid/packer.lua
@@ -13,10 +13,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-return require('lazy').setup({
-  'wbthomason/packer.nvim',
-  'nvim-treesitter/nvim-treesitter-textobjects',
-  'nvim-treesitter/playground',
+require('lazy').setup({
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   'ThePrimeagen/harpoon',
   'ThePrimeagen/vim-be-good',
   'mbbill/undotree',
@@ -41,35 +39,27 @@ return require('lazy').setup({
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  --  The configuration is done below. Search for lspconfig to find it below.
+  { -- LSP Configuration & Plugins
+  'neovim/nvim-lspconfig',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  config = function()
-    pcall(require('nvim-treesitter.install').update { with_sync = true })
-  end,
+    -- Automatically install LSPs to stdpath for neovim
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    -- Useful status updates for LSP
+    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+    { 'j-hui/fidget.nvim', opts = {} },
+    -- Additional lua configuration, makes nvim stuff amazing!
+    'folke/neodev.nvim',
+  }
 },
--- NOTE: This is where your plugins related to LSP can be installed.
---  The configuration is done below. Search for lspconfig to find it below.
-{ -- LSP Configuration & Plugins
-'neovim/nvim-lspconfig',
-dependencies = {
--- Automatically install LSPs to stdpath for neovim
-'williamboman/mason.nvim',
-'williamboman/mason-lspconfig.nvim',
-
--- Useful status updates for LSP
--- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-{ 'j-hui/fidget.nvim', opts = {} },
-
--- Additional lua configuration, makes nvim stuff amazing!
-'folke/neodev.nvim',
-    },
-  },
-  { -- Autocompletion
-  'hrsh7th/nvim-cmp',
-  dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+{ -- Autocompletion
+'hrsh7th/nvim-cmp',
+dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' }
 },
+'nvim-treesitter/nvim-treesitter-textobjects',
+'nvim-treesitter/playground'
 })
 
