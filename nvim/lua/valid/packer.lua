38ebfa7f7db55f5deff3c684fb2592764f7d03ef
Colby <colby.leiske@gmail.com>
2023-04-10 22:31:53 -0700

    change comment plugin

--- nvim/lua/valid/packer.lua
+++ nvim/lua/valid/packer.lua
@@ -28,20 +28,19 @@ return require('packer').startup(function(use)
   use { 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } }
 
   use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use 'nvim-treesitter/playground'
   use 'ThePrimeagen/harpoon'
   use 'ThePrimeagen/vim-be-good'
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-repeat'
   -- gc in V mode to comment
-  -- use 'tpope/vim-commentary'
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-  }
+  use 'tpope/vim-commentary'
+  -- use {
+  --   'numToStr/Comment.nvim',
+  --   config = function()
+  --       require('Comment').setup()
+  --   end
+  -- }
   use 'echasnovski/mini.pairs'
   use 'tpope/vim-sleuth'
   -- Colored brackets
