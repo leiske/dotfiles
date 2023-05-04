39e3c9a8feb1d5a29bb3d1340857128c15356b5f
Colby <colby.leiske@gmail.com>
2023-05-03 23:09:25 -0700

    change harpoon list keymap

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set('n', '<leader>ha', mark.add_file, {desc = "Add file to harpoon list"})
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, { desc = "Show harpoon list"}) 
+vim.keymap.set('n', '<leader>hl', ui.toggle_quick_menu, { desc = "Show harpoon list"})
 
 vim.keymap.set('n', '<C-j>', function() ui.nav_file(1) end, { desc = "Harpoon to first buffer"})
 vim.keymap.set('n', '<C-k>', function() ui.nav_file(2) end, { desc = "Harpoon to second buffer"})
