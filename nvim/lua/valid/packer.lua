2e96386916376b9056ccd8abd42f3982338ee735
Colby <colby.leiske@gmail.com>
2023-07-22 20:55:54 -0700

    remove null ls

--- nvim/lua/valid/packer.lua
+++ nvim/lua/valid/packer.lua
@@ -9,10 +9,10 @@ return require('packer').startup(function(use)
   -- File tree
   -- use('nvim-tree/nvim-tree.lua')
 
-  use {
-    'jose-elias-alvarez/null-ls.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  -- use {
+  --   'jose-elias-alvarez/null-ls.nvim',
+  --   requires = { {'nvim-lua/plenary.nvim'} }
+  -- }
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
