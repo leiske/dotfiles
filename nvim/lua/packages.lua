4a909e8343e51b6a46a0aa2b35da9e0960bf9445
Colby <colby.leiske@gmail.com>
2024-06-25 21:12:31 -0700

    bunch of laptop stuff. god tier commit msg

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    }
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -20,108 +20,108 @@ require('lazy').setup({
     priority = 1000
   },
 
-    -- Useful plugin to show you pending keybinds.
-    {
-      'folke/which-key.nvim',
-      opts = {},
-    },
+  -- Useful plugin to show you pending keybinds.
+  {
+    'folke/which-key.nvim',
+    opts = {},
+  },
 
-    {
-      'github/copilot.vim',
+  {
+    'github/copilot.vim',
+  },
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
     },
-    -- Fuzzy Finder (files, lsp, etc)
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-            -- Only load if `make` is available. Make sure you have the system
-            -- requirements installed.
-            {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                -- NOTE: If you are having trouble with this installation,
-                --       refer to the README for telescope-fzf-native for more instructions.
-                build = 'make',
-                cond = function()
-                    return vim.fn.executable 'make' == 1
-                end,
-            },
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
+    opts = {}
+  },
+
+  -- Git blame and stuff
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {}
+  },
+
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
+
+  -- Adds the middle cmdline and floating notifs 
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        -- bottom_search = true, -- use a classic bottom cmdline for search
+        long_message_to_split = true, -- long messages will be sent to a split
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
     },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- "rcarriga/nvim-notify",
+    }
+  },
 
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            'nvim-tree/nvim-web-devicons'
-        },
-        opts = {}
-    },
-
-    -- Git blame and stuff
-    {
-        'lewis6991/gitsigns.nvim',
-        opts = {}
-    },
-
-    {
-        -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-        },
-        build = ':TSUpdate',
-    },
-
-    -- Adds the middle cmdline and floating notifs 
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            -- add any options here
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                },
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                -- bottom_search = true, -- use a classic bottom cmdline for search
-                long_message_to_split = true, -- long messages will be sent to a split
-                lsp_doc_border = false, -- add a border to hover docs and signature help
-            },
-        },
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-            "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            -- "rcarriga/nvim-notify",
-        }
-    },
-
-    -- quickly switch between windows
-    'ThePrimeagen/harpoon',
-    -- adds more to the . 
-    'tpope/vim-repeat',
-    -- for commenting code
-    'tpope/vim-commentary',
-    -- Open current file in github
-    'ruanyl/vim-gh-line',
-    -- ysawb, my love (surrounding text with quotes, brackets, etc)
-    {
-        "kylechui/nvim-surround",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup();
-        end
-    },
-    -- for different vertical movement when i feel like it
-    'ggandor/leap.nvim',
+  -- quickly switch between windows
+  'ThePrimeagen/harpoon',
+  -- adds more to the . 
+  'tpope/vim-repeat',
+  -- for commenting code
+  'tpope/vim-commentary',
+  -- Open current file in github
+  'ruanyl/vim-gh-line',
+  -- ysawb, my love (surrounding text with quotes, brackets, etc)
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup();
+    end
+  },
+  -- for different vertical movement when i feel like it
+  'ggandor/leap.nvim',
 
   {
     'Wansmer/treesj',
@@ -132,35 +132,35 @@ require('lazy').setup({
     end,
   },
 
-    -- LSP Configuration & Plugins
-    {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            -- Automatically install LSPs to stdpath for neovim
-            { 'williamboman/mason.nvim', config = true },
-            'williamboman/mason-lspconfig.nvim',
+  -- LSP Configuration & Plugins
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
 
-            -- Useful status updates for LSP
-            -- { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      -- Useful status updates for LSP
+      -- { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
 
-            -- Additional lua configuration, makes nvim stuff amazing!
-            'folke/neodev.nvim',
-        },
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
     },
+  },
 
-    {
-        -- Autocompletion
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
 
-            -- Adds LSP completion capabilities
-            'hrsh7th/cmp-nvim-lsp',
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
 
-            -- Adds a number of user-friendly snippets
-            'rafamadriz/friendly-snippets',
-        },
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
     },
+  },
 })
